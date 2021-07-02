d60bce4e34d2171c79b8c35089de863d139a1d8b
prusichka <denchuk.prusak@gmail.com>
2021-07-02 14:48:28 +0300

    reworked prototype

--- 1/Prototype/prototype.ts
+++ 1/Prototype/prototype.ts
@@ -1,20 +1,27 @@
-class Cars {
-  private count: number = 4;
-  user?: string;
-  constructor(user: string) {
-    this.user = user;
+class TeslaCar {
+  model: string;
+  price: number;
+  color: string;
+  autoPilot: boolean;
+  constructor(model: string, price: number, color: string, autoPilot: boolean) {
+    this.model = model;
+    this.price = price;
+    this.color = color;
+    this.autoPilot = autoPilot;
   }
-  outPut(): string {
-    return `${this.user} have ${this.count} cars`;
-  }
-  clone():this {
-    return Object.create(this);
+  produce(): TeslaCar {
+    return new TeslaCar(this.model, this.price, this.color, this.autoPilot);
   }
 }
-const user = new Cars("Denis");
-const userClone = user.clone();
-console.log(user.outPut());
-console.log(userClone.outPut());
+const prototypeCar = new TeslaCar("S", 9999, "black", false);
 
+let car1 = prototypeCar.produce();
+let car2 = prototypeCar.produce();
+let car3 = prototypeCar.produce();
 
+car2.color = "yellow";
+car3.autoPilot = true;
 
+console.log("Car1 ", car1);
+console.log("Car2 ", car2);
+console.log("Car3 ", car3);
