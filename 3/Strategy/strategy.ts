9fb0a19e9f3baaea41f368112318e3b8d3e0e64c
prusichka <denchuk.prusak@gmail.com>
2021-07-04 16:10:47 +0300

    add Strategy

--- 3/Strategy/strategy.ts
+++ 3/Strategy/strategy.ts
@@ -1,20 +1,65 @@
-const firstArray: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const secondArray: number[] = [0, 2, 4, 6, 8];
-
-class ArrayCheck {
-  array: number[];
-  constructor(array: number[]) {
-    this.array = array;
+interface Strategy {
+  priceMenu(): void;
+}
+ 
+class BigMacMenu implements Strategy {
+  priceMenu() : void {
+    console.log('BigMac Menu  \n Big Mac\n CocaCola \n French fries big \n Sauce \n ---10$---');
   }
-  longArray() {
-    if (this.array.length >= 5) {
-      console.log(`${this.array} : is Long`);
-    } else {
-      console.log(`${this.array} : is Short`);
-    }
+}
+class GamburgerMenu implements Strategy {
+  priceMenu() : void {
+    console.log('Gamburger Menu  \n Gamburger\n Sprite \n French fries average  \n Sauce\n ---7$---');
+  }
+}
+class BigTastyMenu implements Strategy {
+  priceMenu() : void {
+    console.log('Big Tasty Menu  \n Big Tasty\n 7-up \n French fries big  \n Sauce x2\n ---13$---');
   }
 }
 
-const z1 = new ArrayCheck(firstArray);
-z1.longArray();
-console.log(z1.array);
+class OrderBurgerMenu {
+  _burgerMenu: Strategy;
+  setMenu(_menu : Strategy) {
+    this._burgerMenu = _menu
+  }
+  price() {
+    this._burgerMenu.priceMenu();
+  }
+}
+
+const orderBurger = new OrderBurgerMenu();
+const orderBurger2 = new OrderBurgerMenu();
+const orderBurger3 = new OrderBurgerMenu();
+const bigMac = new BigMacMenu();
+const gumberger = new GamburgerMenu();
+const bigTasty = new BigTastyMenu();
+//---------------
+orderBurger.setMenu(bigMac);
+orderBurger.price();
+// BigMac Menu
+// Big Mac
+// CocaCola
+// French fries big
+// Sauce
+// ---10$---
+//---------------
+
+//---------------
+orderBurger2.setMenu(gumberger);
+orderBurger2.price();
+// Gamburger Menu
+//  Gamburger
+//  Sprite
+//  French fries average
+//  Sauce
+//  ---7$---
+//---------------
+orderBurger3.setMenu(bigTasty);
+orderBurger3.price();
+// Big Tasty Menu
+//  7-up
+//  French fries big
+//  Sauce x2
+//  ---13$---
+//---------------
