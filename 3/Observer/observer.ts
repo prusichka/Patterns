53325ba82f2262fe437da394950729f996f6ca21
prusichka <denchuk.prusak@gmail.com>
2021-07-04 13:32:10 +0300

    add Observer

--- 3/Observer/observer.ts
+++ 3/Observer/observer.ts
@@ -1,62 +1,35 @@
-interface Target {
-  subscribe(observer : Observer): void;
-  unsubscribe(observer : Observer): void;
-  notify(observer : Observer): void;
-}
-interface Observer {
-
-  update(subject: Target): void;
-}
-
-class ConcretTarget implements Target {
-  static state: number;
-  private observers: Observer[] = [];
-  subscribe(observer: Observer):void {
-    const isExist = this.observers.includes(observer);
-    if (isExist) {
-      return console.log('Subject: Observer has been attached already.');
-    }
-    console.log(`Subject : Attached an observer.`);
-    this.observers.push(observer);
+class FreshNews {
+  subscribes: User[] = [];
+  state: string;
+  subscribe(user: User) {
+    this.subscribes.push(user);
   }
-  unsubscribe(observer: Observer) : void {
-    const observerIndex = this.observers.indexOf(observer)
-    if (observerIndex === -1) {
-      return console.log('Subject: Nonexistent observer.');
-    }
-    this.observers.splice(observerIndex, 1);
-    console.log('Subject: Detached an observer.');
+  setState(_state: string) {
+    this.state = _state;
+    this.notifyAll();
   }
-  notify(observer: Observer) : void {
-    console.log('Subject: Notifying observers...');
-    for (const observer of this.observers) {
-        observer.update(this);
+  notifyAll() {
+    for (const user of this.subscribes) {
+      user.notify(`${user.name} you have fresh news : ${this.state}`);
     }
   }
 }
-
-class ConcreteObserverA implements Observer {
-  public update(subject: Target): void {
-      if (subject instanceof ConcretTarget && ConcretTarget.state < 3) {
-          console.log('ConcreteObserverA: Reacted to the event.');
-      }
+class User {
+  constructor(public name: string) {}
+  notify(message: string) {
+    console.log(message);
   }
 }
 
-class ConcreteObserverB implements Observer {
-  public update(subject: Target): void {
-      if (subject instanceof ConcretTarget && (ConcretTarget.state === 0 || ConcretTarget.state >= 2)) {
-          console.log('ConcreteObserverB: Reacted to the event.');
-      }
-  }
-}
+const user1 = new User("Denis");
+const user2 = new User("Yana");
+const user3 = new User("Stepan");
+const user4 = new User("Sergiy");
 
+const news = new FreshNews();
+news.subscribe(user1);
+news.subscribe(user2);
+news.subscribe(user3);
+news.subscribe(user4);
 
-const subject = new ConcretTarget();
-
-const observer1 = new ConcreteObserverA();
-subject.subscribe(observer1);
-
-const observer2 = new ConcreteObserverB();
-subject.subscribe(observer2);
-subject.unsubscribe(observer2);
+news.setState("You have new subscribes");
